ec3078c985a2f8e87ef7a4004f6751d3244e5d72
zky <100047344+satyaverse@users.noreply.github.com>
2025-09-04 14:58:47 +0800

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+import 'pages/riwayat.dart';
+import 'package:flutter/material.dart';
+import 'pages/item_pages.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'pages/transaksi.dart';
+import 'pages/dashboard.dart';
+//import 'pages/dashboard_page.dart';
+
+
+/// Flutter code sample for [NavigationBar].
+
+void main() {
+  if (Platform.isAndroid || Platform.isIOS) {
+    // Mobile: pakai sqflite default
+  } else {
+    // Desktop: pakai sqflite_common_ffi
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+  runApp(const Dasar());
+}
+
+class Dasar extends StatelessWidget {
+  const Dasar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+    home: NavigationExample());
+  }
+}
+
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    //final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.grey,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home_outlined),
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.add_shopping_cart),
+            label: 'Transaksi',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.history),
+            label: 'Riwayat',
+          ),
+          NavigationDestination(                     
+            selectedIcon: Icon(Icons.app_registration),
+            icon: Icon(Icons.apps),
+            label: 'Produk',
+          ),
+        ],
+      ),
+      
+      body: <Widget>[
+        /// Home page
+        InfoPage(),
+
+        /// Transaksi
+        TransactionPage(),
+
+        ///Riwayat page
+        RiwayatPage(),
+
+        /// Item page
+        ItemPage()
+        
+      ][currentPageIndex],
+    );
+  }
+}
